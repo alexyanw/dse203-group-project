956b81dcfa2fe7f75f559ab125b0af40ea29768d
Jared Goldsmith <jagoldsm@eng.ucsd.edu>
2017-11-22 09:51:05 -0800

    add documentation

--- data_exploration/api/reviews.py
+++ data_exploration/api/reviews.py
@@ -9,6 +9,17 @@ class Reviews(SqlSource, SolrSource):
 
     @log
     def termsByAsin(self, asin):
+        """Given an ASIN, find the terms with the highest Solr score from its reviews.
+
+        Solr score is configurable by facet.method parameter. Default 'fc' option used.
+        https://lucene.apache.org/solr/guide/6_6/faceting.html#Faceting-Thefacet.methodParameter
+
+        Args:
+            asin (string): required. product identifier. Analyze terms for this product's reviews
+
+        Returns:
+             tuple(term, score): term is a token as defined by Solr. score is the metric Solr is configured for (see above).
+        """
         return self._execSolrQuery(
             'asin:"{}"'.format(asin),
             {
@@ -18,6 +29,18 @@ class Reviews(SqlSource, SolrSource):
 
     @log
     def asinByTerms(self, terms):
+        """Given a list of words, find ASIN with the highest Solr score from its reviews.
+
+        Solr score is configurable by facet.method parameter. Default 'fc' option used.
+        https://lucene.apache.org/solr/guide/6_6/faceting.html#Faceting-Thefacet.methodParameter
+
+        Args:
+            terms (list): list of strings
+
+        Returns:
+           tuple(asin, score): asin is a product identifier. score is the metric Solr is configured for (see above).
+        """
+
         return self._execSolrQuery(
             'reviewText:"{}"'.format(','.join(terms)),
             **{
