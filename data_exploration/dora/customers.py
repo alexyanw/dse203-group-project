8409955548aee4dc065d6499b358416b67f29b1a
Laura Wilke <lwilke@eng.ucsd.edu>
2017-11-25 20:13:43 -0800

    clusterCustomers

--- data_exploration/dora/customers.py
+++ data_exploration/dora/customers.py
@@ -33,3 +33,64 @@ class Customers(SqlSource):
                 orderlines.orderid = orders.orderid AND
                 orders.customerid = customers.customerid AND
                 customers.householdid={}'''.format(householdID))
+    
+    @log
+    def clusterQuery(self,min_date='1900-1-1', max_date=None):
+        """For each customer, find the number of books orders, gender, zipcode, household,
+           first name, and total spend on books. 
+
+        Args:
+            max_date (string): optional. date. Limits the timeframe for which 
+            customers results will be returned
+        
+        Returns:
+             tuple(numOrders, gender, zipcode, householdid, firstname, TotalSpent): numOrders 
+             is the number of times a customer has purchased a book. gender is the gender of the 
+             customer. zipcode identifiies the customers location. householdid is the customer's 
+             hosuehold identification. firstname is the customer's name. TotalSpent is the total 
+             amount the customer has spent on books. 
+        """
+        
+        max_date_filter = ' AND o.orderdate <= %(max_date)s' if max_date else ' '
+        query = ( '''
+                  SELECT count(o.orderid) as numOrders, 
+                          c.gender, 
+                          o.zipcode,
+                          c.householdid,
+                          c.firstname,
+                          sum(o.totalprice) as TotalSpent
+                  FROM customers c, orders o
+                  WHERE c.customerid!=0 AND o.customerid=c.customerid 
+                  GROUP BY c.gender, c.householdid, o.zipcode
+                  ORDER BY numOrders desc''')
+        return self._execSqlQuery(query)
+    
+    @log
+    def clusterCustomers(self,n_clusters,algorithm):
+        """Clusters the customers together based on gender, zipcode, numOrders, and TotalSpent. 
+
+        Args:
+            n_clusters (int): required. The number of clusters to form as well 
+            as the number of centroids to generate.
+            algorithm (string): required. “auto”, “full” or “elkan”. K-means algorithm to use. 
+            The classical EM-style algorithm is “full”. The “elkan” variation is more efficient 
+            by using the triangle inequality, but currently doesn’t support sparse data. “auto” 
+            chooses “elkan” for dense data and “full” for sparse data.
+        
+        Returns:
+             tuple(householdid, firstname, y_pred): householdid is the customer's hosuehold
+             identification. firstname is the customer's name. y_pred is the cluster label for 
+             that customer.
+        """
+        
+        data=self.clusterQuery()
+        data=pd.DataFrame(data, data.columns)
+        X=data[['numOrders, gender, zipcode, TotalSpent']].values
+        X=StandardScaler().fit_transform(X)
+        algorithm=KMeans(n_clusters=(n_clusters), algorithm=(algorithm))
+        algorithm.fit_predict(X)
+        y_pred=algorithm.labels_
+        customer=data[['householdid','firstname']]
+        clustering=zip(customer,y_pred)
+        
+        return clustering
