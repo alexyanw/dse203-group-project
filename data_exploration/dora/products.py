1ae0c0566ce8ae381aaaa551c15f4549221982ed
Laura Wilke <lwilke@eng.ucsd.edu>
2017-11-25 20:46:54 -0800

    Add Documentation

--- data_exploration/dora/products.py
+++ data_exploration/dora/products.py
@@ -2,6 +2,7 @@ from .datasources import SqlSource
 from .logger import log
 from datetime import datetime
 
+
 class Products(SqlSource):
     @log
     def ratingsDistribution(self, min_date='1900-1-1', max_date=None, sample_size=100, asin=[]):
@@ -74,6 +75,22 @@ class Products(SqlSource):
 
     @log
     def coPurchases(self, asin, min_date='1900-1-1', max_date=None, sample_size=100 ):
+        """For each given book, find all the books purchased in the same order as the given book 
+        and the number of times that book was purchased.
+
+        Args:
+            asin (list): required. book asin ids. Determines the books that coPurchases will 
+            be searched for. 
+            min_date (string): optional. date. Limits the search result timeframe .
+            max_date (string): optional. date. Limits the search result timeframe.
+            sample_size (int): optional. Percentage of the data the query will run over.
+        
+        Returns:
+             tuple(asin, numPurch): asin is the identification of the book that was purchased 
+             in the same order as one of the input bools. numPurch is the number of times 
+             the book the book was purchased.   
+        """
+        
         if len(asin) == 0: 
             return None
         
@@ -128,15 +145,31 @@ class Products(SqlSource):
                })
         
     @log
-    def clusterQuery(self, min_date='1900-1-1', max_date=None):
+    def clusterQuery(self, min_date='1900-1-1', max_date=None, sample_size=100):
+        """For each book the product id, asin, the number of times it was purchased, 
+        the average star rating, the product category, and days the product has been on sale 
+        is returned.
+
+        Args: 
+            min_date (string): optional. date. Limits the search result timeframe .
+            max_date (string): optional. date. Limits the search result timeframe.
+            sample_size (int): optional. Percentage of the data the query will run over.
+        
+        Returns:
+             tuple(productid, asin, num_orders, avgrating, category, days_on_sale): productid is 
+             the products unqiue identifier. asin is the identification of the book. num_orders 
+             counts the number of times the book has been purchased. avgrating is the average star 
+             rating of the book based on the user reviews. category is the product category that the 
+             book belongs to. days_on_sale is the number of days the book has been on sale.
+        """
+        
         max_date_filter = ' AND o.orderdate <= %(max_date)s' if max_date else ' '
         query = ('''
                   SELECT orderlines.productid,
                       products.asin,
                       count(*) as num_orders,
-                      avg(regexp_replace(orderlines.totalprice :: TEXT, '[$,]', '', 'g') :: NUMERIC) as totalprice_avg,
-                      avg(r.overall),
-
+                      avg(r.overall) as avgrating,
+                      products.nodeid as category,
                       DATE_PART('day', max(shipdate)::TIMESTAMP - min(shipdate)::TIMESTAMP) as days_on_sale
                   FROM orderlines
                       JOIN products
@@ -148,13 +181,35 @@ class Products(SqlSource):
                       JOIN calendar
                         ON o.orderdate=calendar.date
                   WHERE orderlines.numunits > 0
-                  group by orderlines.productid, products.asin;''')
-        return self._execSqlQuery(query,{})
+                  group by orderlines.productid, products.asin, products.nodeid;''')
+        return self._execSqlQuery(query,
+               {
+                    'min_date':min_date,
+                    'max_date':max_date,
+                    'sample_size':sample_size,
+                    'random_seed':self._random_seed
+               })
         
     @log
-    def clusterProducts(self,n_clusters,algorithm,asin=None):
-        data=self.clusterQuery()
-        data=pd.DataFrame(data, data.columns)
+    def clusterProducts(self,n_clusters=8,algorithm='auto',asin=None):
+        """Clusters the books together using KMeans clustering utilizing the clusterQuery 
+        results as the features (num_orders, avgrating, category, and days_on_sale).
+
+        Args: 
+            num_clusters (int): optional. default=8 The number of clusters to form as well as 
+            the number of centroids to generate.
+            algorithm (string): optional. “auto”, “full” or “elkan”, default=”auto”. K-means algorithm 
+            to use. The classical EM-style algorithm is “full”. The “elkan” variation is more efficient
+            by using the triangle inequality, but currently doesn’t support sparse data. “auto” chooses
+            “elkan” for dense data and “full” for sparse data.
+        
+        Returns:
+             tuple(productid, asin, y_pred): productid is the products unqiue identifier. asin is the
+             identification of the book. y_pred is the label of the clsuter that the product belongs to.
+        """
+        response=self.clusterQuery()
+        data=pd.DataFrame(response.results)
+        data.columns=response.columns
         if asin is None:
             input_centers='k-means++'
             X=data[['num_orders','avgrating','category','days_on_sale']].values
@@ -170,8 +225,8 @@ class Products(SqlSource):
         algorithm=KMeans(n_clusters=(n_clusters), algorithm=(algorithm),init=input_centers)
         algorithm.fit_predict(X)
         y_pred=algorithm.labels_
-        prodid=data[['productid']]
-        clustering=zip(prodid,y_pred)
+        clustering=data[['productid','asin']]
+        clustering['y_pred']=y_pred
         
         return clustering
 
