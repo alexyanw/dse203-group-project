fb7978e77d96a12cff721786758cf0c6abb5939e
Jared Goldsmith <jagoldsm@eng.ucsd.edu>
2017-12-04 14:53:03 -0800

    queryresponse visualizations

--- data_exploration/dora/vis.py
+++ data_exploration/dora/vis.py
@@ -5,6 +5,39 @@ class QueryVis:
         self.columns = columns
         self.results = results
 
+    def _data_vectors(self, x, cols):
+        x_index = self.columns.index(x)
+        x_data = [d[x_index] for d in self.results]
+        y_data = []
+        for col in cols:
+            y_index = self.columns.index(col)
+            y_data.append([d[y_index] for d in self.results])
+
+        return (x_data, y_data)
+
+    def scatter(self, x=None, y=None, z=None):
+        data = self._data_vectors(x, [y])
+        plt.scatter(data[0], data[1][0])
+        plt.xlabel(x)
+        plt.ylabel(y)
+
+        plt.show()
+        return
+
+    def line(self, x, cols=None, title=None):
+        cols = (cols
+                if (type(cols) is list) | (type(cols) is tuple)
+                else self.columns)
+
+        data = self._data_vectors(x,cols)
+
+        for i,line in enumerate(data[1]):
+            plt.plot(data[0], line, label=cols[i])
+
+        plt.xlabel(x)
+        plt.title(title)
+        plt.show()
+
     def bar(self, title=None, cols=None):
         cols = (cols
                 if (type(cols) is list) | (type(cols) is tuple)
@@ -15,15 +48,11 @@ class QueryVis:
                    for key in cols
                    ]) for row in self.results
             ]
-        data_y = cols
 
         for row in data_x:
             plt.bar(range(len(cols)), row)
 
-        # set the locations of the xticks
-
-        # set the locations and labels of the xticks
-        plt.xticks(range(len(cols)), cols, rotation=70)
+        plt.xticks(range(len(cols)), cols, rotation=90)
         plt.xlabel('count')
         plt.title(title)
         plt.show()
