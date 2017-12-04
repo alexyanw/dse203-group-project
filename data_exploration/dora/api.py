e8bbaccad17f9665dcfd51d51e5bf501879732c1
Jared Goldsmith <jagoldsm@eng.ucsd.edu>
2017-12-03 18:32:36 -0800

    clean out old test code

--- data_exploration/dora/api.py
+++ data_exploration/dora/api.py
@@ -8,8 +8,9 @@ from .recommendations import Recommendations
 
 from .config import Config
 
+
 class DataExplorer:
-    def __init__(self,config = Config()):
+    def __init__(self, config=Config()):
         self.products = Products(config.sql_config)
         self.reviews = Reviews(config.sql_config, config.solr_config)
         self.orders = Orders(config.sql_config)
@@ -17,36 +18,3 @@ class DataExplorer:
         self.customers = Customers(config.sql_config)
         self.benchmarks = Benchmarks(config.sql_config)
         self.recommendations = Recommendations(config.sql_config)
-
-if __name__ == '__main__':
-    explorer = DataExplorer()
-    #stats = explorer.orders.statsByZipcode()
-    #print(stats.columns)
-    #print(stats.results)
-
-    dist = explorer.products.clusterQuery()
-    print(dist.results)
-    #
-    # terms = explorer.reviews.termsByAsin(asin=['0007386648'])
-    # print(terms.columns)
-    # print(terms.results)
-    #
-    #
-    # dist2 = explorer.products.ratingsDistribution(asin=('0007386648', '0002007770'))
-    # print(dist2.results)
-    #
-    # orders = explorer.orders.statsByZipcode()
-    # print(orders.results)
-    #
-    # cat = explorer.categories.search('Architecture', classfication_only=True)
-    # print(cat.results)
-    #
-    # cat = explorer.categories.search('Architecture', classfication_only=True)
-    # print(cat.results)
-    #
-    # terms = explorer.reviews.termsByAsin(asin=['0007386648'])
-    # print(terms.columns)
-    # print(terms.results)
-
-    #cat = explorer.categories.parentsOf(173508)
-    #print(cat.results)
